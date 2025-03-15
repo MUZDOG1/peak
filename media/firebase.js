59911dfab9f1189556289f6a282cbddeb09d574e
muzdog <murray.loryman@preshil.vic.edu.au>
2025-03-15 22:08:41 +1100

    Add files via upload

---
+++ media/firebase.js
@@ -0,0 +1,21 @@
+// Import Firebase functions
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, serverTimestamp, query, orderBy, onSnapshot } from "firebase/firestore";
+
+// Firebase configuration (the one you got)
+const firebaseConfig = {
+  apiKey: "AIzaSyAr_YalGc9rlZijAY17uQPAT2PyxfMiD-8",
+  authDomain: "chatroom-80c45.firebaseapp.com",
+  projectId: "chatroom-80c45",
+  storageBucket: "chatroom-80c45.firebasestorage.app",
+  messagingSenderId: "810230295758",
+  appId: "1:810230295758:web:d736c80d30f0d83b19749f",
+  measurementId: "G-4973KL5NCN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Export Firestore database
+export { db };
